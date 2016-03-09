22aa25b21a9e9f4d7116896e39e7bf206eb8c504
geemili <minergeemili@gmail.com>
2016-03-09 17:44:49 -0600

    Converted program to C++

--- src/robot/configure.ino
+++ src/robot/configure.ino
@@ -2,35 +2,68 @@
 #define CONFIGURE_MAX_MOTORS 8
 #define CONFIGURE_MAX_SENSORS 1
 
-struct Motor {
+class Motor {
 	byte pinA;
 	byte pinB;
 	byte pinPWM;
 };
 
-struct Sensor {
+class Sensor {
 	byte pin;
 	bool state;
 };
 
 class Robot {
-	int maximum_pwm;
-	int minimum_pwm;
+	byte maximum_pwm;
+	byte minimum_pwm;
 	int safety_timeout;
 	Motor motors[CONFIGURE_MAX_MOTORS];
 	Sensor sensors[CONFIGURE_MAX_SENSORS];
 	byte multiplexerPins[4];
 
-	public:
-		new();
+  public:
+	Robot();
+	void setPWMBounds(byte, byte);
+	void setSafetyTimeout(int);
+	void setMultiplexerPins(byte, byte, byte, byte);
+	Motor getMotor(int);
+	Sensor getSensor(int);
 };
 
-public Robot::new() {
-	Robot robot;
+Robot::Robot() {
+	maximum_pwm = 175;
+	minimum_pwm = 10;
+	safety_timeout = 2;
 
-	robot.maximum_pwm = 175;
-	robot.minimum_pwm = 10;
-	robot.safety_timeout = 2;
+	for (int i=0; i<CONFIGURE_MAX_MOTORS; i++) {
+		motors[i] = Motor{};
+	}
 
-	return robot;
+	for (int i=0; i<CONFIGURE_MAX_SENSORS; i++) {
+		sensors[i] = Sensor{};
+	}
+}
+
+void Robot::setPWMBounds(byte low, byte high) {
+	minimum_pwm = low;
+	maximum_pwm = high;
+}
+
+void Robot::setSafetyTimeout(int timeout) {
+	safety_timeout = timeout;
+}
+
+void Robot::setMultiplexerPins(byte one, byte two, byte three, byte four) {
+	multiplexerPins[0] = one;
+	multiplexerPins[1] = two;
+	multiplexerPins[2] = three;
+	multiplexerPins[3] = four;
+}
+
+Motor Robot::getMotor(int id) {
+	return motors[id];
+}
+
+Sensor Robot::getSensor(int id) {
+	return sensors[id];
 }
